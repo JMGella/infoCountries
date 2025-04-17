a21a08c0f49a4aad52ca0f334b51cd16fccc5115
JMGella <151771028+JMGella@users.noreply.github.com>
2025-04-17 11:49:39 +0200

    añadido fxml main view

--- src/main/java/org/example/infocountries/controller/AppController.java
+++ src/main/java/org/example/infocountries/controller/AppController.java
@@ -1,15 +1,31 @@
 package org.example.infocountries;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TextField;
+import org.w3c.dom.Text;
 
 public class AppController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField tfApiSearch;
+    @FXML
+    private Button btSearchByName;
+    @FXML
+    private Button btSearchByRegion;
+    @FXML
+    private TabPane tpResults;
+
+    @FXML
+    protected void searchByName(){
+
+    }
+
+    @FXML
+    protected void searchByRegion(){
+
     }
 
 
